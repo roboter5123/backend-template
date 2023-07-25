e9486d447c5459dc799dd7b5bedee6cbba0547e9
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2023-07-25 14:10:54 +0200

    Update CorsConfig.java

--- src/main/java/com/roboter5123/{projectName}/config/CorsConfig.java
+++ src/main/java/com/roboter5123/{projectName}/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
 
 
-        registry.addMapping("/**")
+        registry.addMapping("/*")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
