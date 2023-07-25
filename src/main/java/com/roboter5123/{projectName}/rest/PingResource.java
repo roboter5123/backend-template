6b2a15ac6a7db5ca9e6b747f6a7db5861a3a3c3c
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2023-07-25 15:23:30 +0200

    Update PingResource.java

--- src/main/java/com/roboter5123/{projectName}/rest/PingResource.java
+++ src/main/java/com/roboter5123/{projectName}/rest/PingResource.java
@@ -1,12 +1,5 @@
 package at.dbeg.eos2.service.sandbox.campbookpascalgehringer.rest;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
