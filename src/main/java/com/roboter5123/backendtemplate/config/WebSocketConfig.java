c5e7874e647ce0079388441ca22ef739d9dbc6cc
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2023-07-28 11:13:13 +0200

    Update WebSocketConfig.java

--- src/main/java/com/roboter5123/backendtemplate/config/WebSocketConfig.java
+++ src/main/java/com/roboter5123/backendtemplate/config/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.roboter5123.backend-template;
+package com.roboter5123.backendtemplate;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
