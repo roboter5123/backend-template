1890f5b6686283adcd791d350d74a17b07fd963e
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2023-07-28 11:10:37 +0200

    Add files via upload

---
+++ src/main/java/com/roboter5123/backendtemplate/rest/PingResource.java
@@ -0,0 +1,19 @@
+package com.roboter5123.backendtemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.roboter5123.template.rest.model.Ping;
+
+@RestController
+@RequestMapping("/ping")
+public class PingResource {
+
+    @GetMapping()
+    @ResponseBody
+    public Ping ping() {
+
+        return new Ping();
+    }
+
+}
