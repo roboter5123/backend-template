84b686edee1bdf7c618e7e4a7e9ebfa8242b41d6
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2023-07-28 09:53:02 +0200

    Add files via upload

---
+++ src/main/java/com/pixelthump/template/config/ModelMapperConfig.java
@@ -0,0 +1,13 @@
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    ModelMapper getModelMapper() {
+
+        return new ModelMapper();
+    }
+}
