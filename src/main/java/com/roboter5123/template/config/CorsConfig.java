9c8e5090e17c849d9799881abc9b3603f4fd655a
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2023-07-28 10:18:54 +0200

    Update CorsConfig.java

--- src/main/java/com/roboter5123/template/config/CorsConfig.java
+++ src/main/java/com/roboter5123/template/config/CorsConfig.java
@@ -1,3 +1,4 @@
+package com.roboter5123.template.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
