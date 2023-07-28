172c4ce0ffed4734b62df3ed3369fa62bc4e09a1
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2023-07-28 11:19:15 +0200

    Update PingResource.java

--- src/main/java/com/roboter5123/backendtemplate/rest/PingResource.java
+++ src/main/java/com/roboter5123/backendtemplate/rest/PingResource.java
@@ -1,4 +1,4 @@
-package com.roboter5123.backendtemplate;
+package com.roboter5123.backendtemplate.rest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
