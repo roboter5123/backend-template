a43087b0a727f7bb5d8be29070dc6614d1bca2ba
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2023-07-28 09:57:29 +0200

    Add files via upload

---
+++ src/main/java/com/roboter5123/template/rest/model/Ping.java
@@ -0,0 +1,17 @@
+import java.time.OffsetDateTime;
+
+/**
+ * Ping response with the current time.
+ */
+public class Ping {
+
+	private OffsetDateTime time;
+
+	public Ping() {
+		time = OffsetDateTime.now();
+	}
+
+	public OffsetDateTime getTime() {
+		return time;
+	}
+}
