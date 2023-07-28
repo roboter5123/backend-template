bafde038f9edc6d6137fab927d52fa34e5a5e8a1
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2023-07-28 10:25:40 +0200

    Update WebSocketConfig.java

--- src/main/java/com/roboter5123/template/config/WebSocketConfig.java
+++ src/main/java/com/roboter5123/template/config/WebSocketConfig.java
@@ -1,3 +1,5 @@
+package com.roboter5123.template.config;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
