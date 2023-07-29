b5d288cd31a72b807ce1ce2b122655129e9e6e42
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2023-07-29 18:53:13 +0200

    Update Application.java

--- src/main/java/com/roboter5123/backendtemplate/Application.java
+++ src/main/java/com/roboter5123/backendtemplate/Application.java
@@ -3,6 +3,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class Application {
 
 	public static void main(String[] args) {
