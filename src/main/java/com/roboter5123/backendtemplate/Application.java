c9d2927b7c534daa33a1df72650e0c554ff8149a
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2023-07-29 18:55:11 +0200

    Update Application.java

--- src/main/java/com/roboter5123/backendtemplate/Application.java
+++ src/main/java/com/roboter5123/backendtemplate/Application.java
@@ -1,4 +1,5 @@
 package com.roboter5123.backendtemplate;
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
