d4120983776292ca40fb961cc8aa1bc32d9f90fa
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2023-07-31 10:55:49 +0200

    Update TestApplication.java

--- src/main/java/com/roboter5123/backendtemplate/TestApplication.java
+++ src/main/java/com/roboter5123/backendtemplate/TestApplication.java
@@ -1,7 +1,9 @@
 package com.roboter5123.backendtemplate;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = {
