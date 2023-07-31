8530c1c950dad60a9734ed565c876c9b70b798c5
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2023-07-31 11:14:57 +0200

    Update PingResourceTest.java

--- src/test/java/PingResourceTest.java
+++ src/test/java/PingResourceTest.java
@@ -4,12 +4,19 @@ import com.roboter5123.backendtemplate.rest.model.Ping;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
 
 import java.time.OffsetDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(classes = TestApplication.class)
+@DataJpaTest
+@TestPropertySource(properties = {
+    "spring.jpa.hibernate.ddl-auto=none", // Disable auto DDL generation
+    "spring.datasource.url=" // Set an empty URL to disable the default data source
+})
 class PingResourceTest {
 
     @Autowired
