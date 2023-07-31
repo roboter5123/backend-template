caa91c04f16b4aa76d1a3f044cc9c555cc202cbe
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2023-07-31 08:39:31 +0200

    Update Application.java

--- src/main/java/com/roboter5123/backendtemplate/Application.java
+++ src/main/java/com/roboter5123/backendtemplate/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableEncryptableProperties
+@EnableScheduling
 public class Application {
 
 	public static void main(String[] args) {
